d8b60415e2494de31ecafd0cfda20e2787f74ac7
joyang1 <tingzai.yang@gmail.com>
2019-03-28 23:43:09 +0800

    init valid BST in leetcode.

---
+++ leetcode/src/main/java/cn/tommyyang/tree/ValidBST.java
@@ -0,0 +1,18 @@
+package cn.tommyyang.tree;
+
+/**
+ * @author TommyYang on 2019-03-28
+ */
+public class ValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        if(root == null){
+            return false;
+        }
+
+
+
+        return false;
+    }
+
+}

0bef8b685632dd0e2a80447738a7f44a5c81747a
joyang1 <tingzai.yang@gmail.com>
2019-03-29 23:43:37 +0800

    add valid BST in leetcode.

--- leetcode/src/main/java/cn/tommyyang/tree/ValidBST.java
+++ leetcode/src/main/java/cn/tommyyang/tree/ValidBST.java
@@ -5,13 +5,35 @@ package cn.tommyyang.tree;
  */
 public class ValidBST {
 
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(10);
+        TreeNode t2 = new TreeNode(5);
+
+        TreeNode t3 = new TreeNode(15);
+        TreeNode t4 = new TreeNode(6);
+        TreeNode t5 = new TreeNode(20);
+
+        t1.left = t2;
+        t1.right = t3;
+        t3.left = t4;
+        t3.right = t5;
+
+        new ValidBST().isValidBST(t1);
+    }
+
+
+    double last = -Double.MAX_VALUE;
     public boolean isValidBST(TreeNode root) {
-        if(root == null){
-            return false;
+        if (root == null) {
+            return true;
         }
-
-
-
+        if (isValidBST(root.left)) {
+            if (last < root.val) {
+                last = root.val;
+                return isValidBST(root.right);
+            }
+        }
         return false;
     }
 

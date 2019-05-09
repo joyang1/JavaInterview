be68ae2413b2120b7458d0861dc074e05d92a065
joyang1 <tingzai.yang@gmail.com>
2019-05-09 23:21:06 +0800

    add grpc client error status test in test module.

---
+++ test/src/main/java/cn/tommyyang/grpc/GrpcClient.java
@@ -0,0 +1,61 @@
+package cn.tommyyang.grpc;
+
+import cn.tommyyang.grpc.protobuf.GreeterGrpc;
+import cn.tommyyang.grpc.protobuf.GreeterOuterClass;
+import com.google.protobuf.Any;
+import io.grpc.*;
+import io.grpc.protobuf.lite.ProtoLiteUtils;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author TommyYang on 2019-05-09
+ */
+public class GrpcClient {
+
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public GrpcClient(String host, int port) {
+        this.channel = ManagedChannelBuilder.forAddress(host, port)
+                .usePlaintext().build();
+        this.blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+
+    public void greet() throws IOException {
+        GreeterOuterClass.Request request = GreeterOuterClass.Request
+                .newBuilder()
+                .setName("aaa")
+                .build();
+
+        try {
+            GreeterOuterClass.Response response = this.blockingStub.hello(request);
+            System.out.println(response.getMsg());
+        } catch (StatusRuntimeException e){
+            System.out.println(e.toString());
+            System.out.println(e.getStatus().getCode());
+            Metadata metadata = e.getTrailers();
+            com.google.rpc.Status status = metadata.get(Metadata.Key.of("grpc-status-details-bin",
+                    ProtoLiteUtils.metadataMarshaller(com.google.rpc.Status.getDefaultInstance())));
+            if(status.getDetailsCount() > 0){
+                Any any = status.getDetails(0);
+                GreeterOuterClass.Request req = any.unpack(GreeterOuterClass.Request.class);
+                System.out.println(req.getName());
+            }
+
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        GrpcClient client = new GrpcClient("localhost", 50051);
+        client.greet();
+    }
+
+}
---
+++ test/src/main/java/cn/tommyyang/grpc/GrpcServer.java
@@ -0,0 +1,86 @@
+package cn.tommyyang.grpc;
+
+import cn.tommyyang.grpc.protobuf.GreeterGrpc;
+import cn.tommyyang.grpc.protobuf.GreeterOuterClass;
+import com.google.protobuf.Any;
+import io.grpc.Metadata;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.Status;
+import io.grpc.protobuf.lite.ProtoLiteUtils;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+/**
+ * @author TommyYang on 2019-05-09
+ */
+//
+public class GrpcServer {
+
+    private static final Metadata.Key<com.google.rpc.Status> STATUS_DETAILS_KEY =
+            Metadata.Key.of(
+                    "grpc-status-details-bin",
+                    ProtoLiteUtils.metadataMarshaller(com.google.rpc.Status.getDefaultInstance()));
+    private Server server;
+
+    private void start() throws IOException {
+        int port = 50051;
+
+        server = ServerBuilder.forPort(port)
+                .addService(new GreeterImpl())
+                .build()
+                .start();
+
+        System.out.println("server start at:" + 50051);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run(){
+                GrpcServer.this.stop();
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final GrpcServer server = new GrpcServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase{
+        @Override
+        public void hello(GreeterOuterClass.Request request, StreamObserver<GreeterOuterClass.Response> responseObserver) {
+            try {
+                GreeterOuterClass.Response response = GreeterOuterClass.Response.newBuilder()
+                        .setMsg("hello" + request.getName()).build();
+                //responseObserver.onNext(response);
+                //responseObserver.onCompleted();
+                throw new Exception("error");
+            } catch (Exception e){
+                Metadata metadata = new Metadata();
+                com.google.rpc.Status status = com.google.rpc.Status.newBuilder()
+                        .addDetails(Any.pack(request))
+                        .build();
+                metadata.put(STATUS_DETAILS_KEY, status);
+                Exception ex = Status.INTERNAL.withCause(e).withDescription("internal error").asRuntimeException(metadata);
+                responseObserver.onError(ex);
+            }
+
+        }
+    }
+}
